d69c09dea66839188d4476937043d22c9fa0dc33
S. Shibasaki <sdeb6vm@gmail.com>
2025-04-15 00:14:57 +0900

    初期プロジェクト設定の追加
    - Visual Studio ソリューションの作成
    - DataLoader: JVLinkClass を使用する C++/CLI プロジェクト設定
    - Trainer: 基本的な C++ コンソールアプリケーションのスケルトン追加
    - .gitignore ファイルによるビルド成果物の除外設定
    
    このコミットはプロジェクトの基本的な構造を確立します。DataLoader はJRA-VAN データラボの
    データにアクセスするためのインターフェースとして機能し、Trainer プロジェクトは後に
    分析機能を実装するための土台となります。

---
+++ DataLoader/DataLoader.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+
+using namespace System;
+using namespace JVDTLabLib;
+
+int main(array<System::String ^> ^args)
+{
+	JVLinkClass^ jvLink = gcnew JVLinkClass();
+	Console::WriteLine("JVLinkClass loaded successfully.");
+    return 0;
+}

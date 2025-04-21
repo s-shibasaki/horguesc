588d7e37446851ce86eec0a95454f15c631f3f23
S. Shibasaki <sdeb6vm@gmail.com>
2025-04-21 12:42:34 +0900

    Add Program.cpp entry point for Windows Forms application
    - Created main entry point for C++/CLI Windows Forms application
    - Configured visual styles and text rendering defaults
    - Instantiated and launched the MainForm component

---
+++ dataloader/Program.cpp
@@ -0,0 +1,16 @@
+#include "MainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace dataloader;
+
+[STAThread]
+int main(array<String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    // メインフォームの作成と実行
+    Application::Run(gcnew MainForm());
+    return 0;
+}

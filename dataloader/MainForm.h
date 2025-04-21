310dbabed2bc652c5f3f408046341d7e5e1960f5
S. Shibasaki <sdeb6vm@gmail.com>
2025-04-21 12:47:44 +0900

    Change window title from "MainForm" to "DataLoader"
    - Updated the window title text in MainForm.h to match application name
    - Added Program.cpp to the project file

--- dataloader/MainForm.h
+++ dataloader/MainForm.h
@@ -50,7 +50,7 @@ namespace dataloader {
 		{
 			this->components = gcnew System::ComponentModel::Container();
 			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MainForm";
+			this->Text = L"DataLoader";
 			this->Padding = System::Windows::Forms::Padding(0);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 		}
